e6d0535e9ca85cb963c9bfd81164b87864f7d975
Bulgan Erdenebaatar <bulganebaatar@gmail.com>
2026-06-12 12:56:21 -0700

    repo created and working first skeleton

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Hero } from "@/sections/Hero";
+import { About } from "@/sections/About";
+import { Projects } from "@/sections/Projects";
+import { Experience } from "@/sections/Experience";
+import { Navbar } from "@/layout/Navbar";
+function App() {
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Experience />
+      </main>
+    </div>
+  );
+}
+
+export default App;
